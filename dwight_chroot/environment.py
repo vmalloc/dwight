a64ce5c8f965ca5ddc7bd22e88909e3cbc213568
Rotem Yaari <vmalloc@gmail.com>
2012-07-25 00:33:18 +0300

    Move execute_command and execute_command_assert_success to platform_utils

--- dwight_chroot/environment.py
+++ dwight_chroot/environment.py
@@ -14,12 +14,15 @@ from .cache import Cache
 from .include import Include
 from .exceptions import (
     CannotLoadConfiguration,
-    CommandFailed,
     InvalidConfiguration,
     NotRootException,
     UnknownConfigurationOptions,
     )
-from .platform_utils import get_user_shell
+from .platform_utils import (
+    execute_command,
+    execute_command_assert_success,
+    get_user_shell,
+    )
 from .resources import Resource
 
 _logger = logging.getLogger(__name__)
@@ -62,7 +65,7 @@ class Environment(object):
         self._unshare_mount_points()
         path = self._mount_base_image()
         self._mount_includes(path)
-        p = self._execute_command("env {env} /usr/sbin/chroot {path} {cmd}".format(
+        p = execute_command("env {env} /usr/sbin/chroot {path} {cmd}".format(
             env=" ".join('{0}="{1}"'.format(key, value) for key, value in self.environ.iteritems()),
             path=path,
             cmd=cmd))
@@ -76,7 +79,7 @@ class Environment(object):
         path = mkdtemp()
         _logger.debug("Mounting base image %r in %r", self.base_image, path)
         base_image_path = self.base_image.get_path(self)
-        self._execute_command_assert_success("mount -n -t squashfs -o loop {0} {1}".format(base_image_path, path))
+        execute_command_assert_success("mount -n -t squashfs -o loop {0} {1}".format(base_image_path, path))
         return path
     def _mount_includes(self, base_path):
         for mount_point, include in self.includes.iteritems():
@@ -89,16 +92,7 @@ class Environment(object):
             mount_point = os.path.relpath(mount_point, '/')
         mount_point = os.path.join(base_path, mount_point)
         _logger.debug("Mounting (binding) %r to %s", path, mount_point)
-        self._execute_command_assert_success("mount -n --bind {0} {1}".format(path, mount_point))
-    def _execute_command_assert_success(self, cmd, **kw):
-        returned = self._execute_command(cmd, **kw)
-        if returned.returncode != 0:
-            raise CommandFailed("Command {0!r} failed with exit code {1}".format(cmd, returned))
-        return returned
-    def _execute_command(self, cmd, **kw):
-        returned = subprocess.Popen(cmd, shell=True, **kw)
-        returned.wait()
-        return returned
+        execute_command_assert_success("mount -n --bind {0} {1}".format(path, mount_point))
 
 
                 
--- dwight_chroot/platform_utils.py
+++ dwight_chroot/platform_utils.py
@@ -1,5 +1,19 @@
 import os
 import pwd
+import subprocess
+from .exceptions import CommandFailed
     
 def get_user_shell():
     return pwd.getpwuid(os.getuid()).pw_shell
+
+def execute_command_assert_success(cmd, **kw):
+    returned = execute_command(cmd, **kw)
+    if returned.returncode != 0:
+        raise CommandFailed("Command {0!r} failed with exit code {1}".format(cmd, returned))
+    return returned
+
+def execute_command(cmd, **kw):
+    returned = subprocess.Popen(cmd, shell=True, **kw)
+    returned.wait()
+    return returned
+

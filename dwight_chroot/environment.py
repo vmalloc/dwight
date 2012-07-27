a14b99e1bd5be70f9850a100671d7e8b2265c224
Rotem Yaari <vmalloc@gmail.com>
2012-07-27 23:46:27 +0300

    Move to use a forked child for the actual chrooting/unsharing

--- dwight_chroot/environment.py
+++ dwight_chroot/environment.py
@@ -3,6 +3,7 @@ import os
 import platform
 import string
 import subprocess
+import sys
 from tempfile import mkdtemp
 
 if platform.system().lower() == "linux":
@@ -34,7 +35,6 @@ class Environment(object):
         super(Environment, self).__init__()
         self.reset_configuration()
         self.cache = Cache(os.path.expanduser("~/.dwight-cache"))
-        self._active_mounts = []
     def reset_configuration(self):
         self.base_image = None
         self.includes = []
@@ -62,21 +62,34 @@ class Environment(object):
     ############################################################################
     def run_shell(self):
         self.run_command_in_chroot(get_user_shell())
-    def run_command_in_chroot(self, cmd, cleanup_mounts=False):
+    def run_command_in_chroot(self, cmd):
         if os.getuid() != 0:
             raise NotRootException("Dwight must be run as root")
+        child_pid = os.fork()
+        if child_pid == 0:
+            self._run_command_in_chroot_as_forked_child(cmd)
+        else:
+            return self._wait_for_forked_child(child_pid)
+    def _run_command_in_chroot_as_forked_child(self, cmd):
         try:
             self._unshare_mount_points()
             path = self._mount_base_image()
             self._mount_includes(path)
-            p = execute_command("env {env} /usr/sbin/chroot {path} {cmd}".format(
-                env=" ".join('{0}="{1}"'.format(key, value) for key, value in iteritems(self.environ)),
-                path=path,
-                cmd=cmd))
-        finally:
-            if cleanup_mounts:
-                self._cleanup_mounts()
-        return p
+            p = execute_command(
+                "env {env} /usr/sbin/chroot {path} {cmd}".format(
+                    env=" ".join('{0}="{1}"'.format(key, value) for key, value in iteritems(self.environ)),
+                    path=path,
+                    cmd=cmd)
+                    )
+            os._exit(p.wait())
+        except Exception:
+            _logger.error("Error occurred running command", exc_info=True)
+            raise
+    def _wait_for_forked_child(self, child_pid):
+        _, exit_code = os.waitpid(child_pid, 0)
+        exit_code &= 0xff
+        _logger.debug("_wait_for_forked_child: child returned %s", exit_code)
+        return exit_code
     def _unshare_mount_points(self):
         if unshare is None:
             raise PlatformNotSupported("{0} is not supported".format(platform.system()))
@@ -93,10 +106,6 @@ class Environment(object):
             _logger.debug("Fetching include %s...", include)
             path = include.to_resource().get_path(self)
             self._mount_path(path, base_path, include.dest)
-    def _cleanup_mounts(self):
-        while self._active_mounts:
-            execute_command_assert_success("umount -f {0}".format(self._active_mounts[-1]))
-            self._active_mounts.pop(-1)
     def _mount_path(self, path, base_path, mount_point):
         path = os.path.abspath(path)
         if os.path.isabs(mount_point):
@@ -110,8 +119,6 @@ class Environment(object):
     def _mount_regular_file(self, path, mount_point):
         _logger.debug("Mounting squashfs file %r to %s", path, mount_point)
         execute_command_assert_success("mount -n -t squashfs -o loop {0} {1}".format(path, mount_point))
-        self._active_mounts.append(mount_point)
     def _mount_directory(self, path, mount_point):
         _logger.debug("Mounting (binding) %r to %s", path, mount_point)
         execute_command_assert_success("mount -n --bind {0} {1}".format(path, mount_point))
-        self._active_mounts.append(mount_point)
